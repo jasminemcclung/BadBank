32d89a088dd8af4d95eb24cec96cc4008112bc89
Jasmine McClung <103603599+jasminemcclung@users.noreply.github.com>
2022-10-25 18:34:44 -0500

    commit

--- public/deposit.js
+++ public/deposit.js
@@ -1,21 +1,20 @@
-function Deposit(){
+function deposit(){
   return (
     <Card
     bgcolor="dark"
      txtcolor="light"
-     header="deposit"
-     title= "Make a deposit"
+     header="Make a Deposite"
+     
      text=""
-     
+     body={<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+    Deposite
+   </button>
 
-     
+     }
 
-     
-
-    />  )
+    /> 
+  )
 }
-
-
 function Balance(){
   return (
     <h1>Balance</h1>
